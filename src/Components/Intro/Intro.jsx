f32a5095fa3de556b22ecdd677278e0febbebba9
SewGit <it20100902@my.sliit.lk>
2022-10-10 16:50:14 +0530

    initial commit

---
+++ src/Components/Intro/Intro.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Intro = () => {
+    <div className="intro">
+        <div className="left">
+            <div className="name">
+                <span>Hy I Am </span>
+                <span>Sewwandi Maheshika</span>
+                <span> Web Developer </span>
+                <span>I'm 3rd year undergratuate student at SLIIT Srilanka Istitute of Information Technology.
+                    I'm seeing and internship position in this industry.
+                     </span>
+            </div>
+            <div className="i-button">Hire me</div>
+        </div>
+        <div className="right"></div>
+        
+    </div>
+}
+
+export default Intro;
